0599cc96576db520b58fd8d69f89b71b583f779d
yaswanth1199 <51701963+yaswanth1199@users.noreply.github.com>
2019-06-23 12:32:55 +0530

    Create 3.c

---
+++ 3.c
@@ -0,0 +1,18 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+    int a[100];
+   int n,j=0;
+   scanf("%d",&n);
+   while(j<n)
+   {
+       scanf("%d",&a[j]);
+   
+       ++j;
+   }
+ 
+    printf("%d",a[0]);
+}
