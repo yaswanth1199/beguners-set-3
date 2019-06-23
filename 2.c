4a51b61a425ca3f080253919f451a210db33de96
yaswanth1199 <51701963+yaswanth1199@users.noreply.github.com>
2019-06-23 12:27:09 +0530

    Update 2.c

--- 2.c
+++ 2.c
@@ -4,13 +4,15 @@
 
 int main()
 {
-   int n,i=1;
+    int a[100];
+   int n,j=0;
    scanf("%d",&n);
-  while(i<=n)
-  {
-      printf("%d ",i);
-      ++i;
-      
-  }
+   while(j<n)
+   {
+       scanf("%d",&a[j]);
    
+       ++j;
+   }
+ 
+    printf("%d",a[n-1]);
 }
